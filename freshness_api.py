1f3ffd32d5ebb40bb36bfbbc0be08a2c0bdfca9e
AISHIK DAS <101806177+Aishik12-bit@users.noreply.github.com>
2023-07-12 15:18:19 +0530

    .

--- freshness_api.py
+++ freshness_api.py
@@ -96,5 +96,6 @@ def classify():
     return jsonify({'prediction': str(is_fresh), 'freshness':ret_fresh(is_fresh), 'type':pred_type})
 
 
+
 if __name__ == '__main__':
     app.run(host='0:0:0:0', port=4000,debug=True)
